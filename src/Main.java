cba8415ba5670173f43c51618e8a8f792df0b8a0
Murad <ashrafov.main@gmail.com>
2024-10-18 02:18:51 +0400

    method

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Write the length of array");
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int[] arr = new int[length];
+        for (int i = 0; i < length; i++) {
+            System.out.println("Enter element " + i + ":");
+            arr[i] = sc.nextInt();
+        }
+        for (int i = 0; i < length; i++) {
+            boolean iscompound =iscompound(arr[i]);
+            if (!iscompound) {
+                System.out.println(arr[i]);
+            }
+        }
+    }
 
-        Scanner scanner = new Scanner(System.in);
-
-        long a = scanner.nextLong();
-        long p = a * 4;
-        long s = (long)Math.pow (2,2);
-
-        System.out.println(s);
-        System.out.println(p);
-
-
+    private static boolean iscompound(int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) {
+                return true;
+            }
+        }
+        return false;
     }
 }
